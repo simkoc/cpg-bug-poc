63d4aba85e17648c48f2ef801300574d432c06cf
Simon Koch <simon.koch@tu-braunschweig.de>
2020-11-29 18:45:19 +0100

    moved file into correct folder

--- src/main/scala/de/tubs/DeleteExtStmtCalls.scala
+++ src/main/scala/de/tubs/DeleteExtStmtCalls.scala
@@ -1,9 +1,9 @@
 package de.tubs
 
 import io.shiftleft.codepropertygraph.Cpg
-import io.shiftleft.semanticcpg.language._
 import io.shiftleft.codepropertygraph.generated.{EdgeTypes, nodes}
 import io.shiftleft.passes.{DiffGraph, IntervalKeyPool, ParallelCpgPass}
+import io.shiftleft.semanticcpg.language._
 
 import scala.jdk.CollectionConverters._
 
